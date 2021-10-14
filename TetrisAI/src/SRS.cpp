6b9bf95dea6bc0f72b03dcceb4ed5af323d3c0c2
Ryoga.exe <ryoga.dev@gmail.com>
2021-10-15 01:47:07 +0900

    Add SRS: Super Rotation System

---
+++ TetrisAI/src/SRS.cpp
@@ -0,0 +1,19 @@
+﻿#include "SRS.hpp"
+
+namespace SRS {
+    bool Rotate(Stage& stage, Mino& mino, bool clockwise) {
+        if (mino.type() == 1) return false; // O-mino
+
+        for (uint32 i = 0; i < SRSCheckNum; i++) {
+            Mino candidate = mino.rotated(clockwise).moved(SRSMovePos[mino.type() != 0][!clockwise][mino.angle()][i]);
+
+            if (!stage.isHit(candidate)) {
+                // return candidate;
+                mino = candidate;
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
