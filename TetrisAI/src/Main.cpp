ded450ee391ceac27e5ff3575109043c9182cad3
Ryoga.exe <ryoga.dev@gmail.com>
2021-10-28 23:39:04 +0900

    Update: set end_of_line config

--- TetrisAI/src/Main.cpp
+++ TetrisAI/src/Main.cpp
@@ -5,8 +5,18 @@
 void Main() {
     Tetris tetris;
     KeyConfig keycon;
+    int32 index = 0;
     while (System::Update()) {
+        ClearPrint();
+        Print << index;
         tetris.update(keycon);
         tetris.draw();
+
+        auto res = tetris.getAllPlaceable();
+
+        if (0 <= index && index < res.size()) tetris.drawMino(res[index], ColorF(1.0, 0.3));
+
+        if (Key1.down()) index++;
+        if (Key2.down()) index--;
     }
 }
